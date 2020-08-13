84d0cbe8d45352fb64eb3acf5bea57c91a4f225a
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-08-13 11:28:51 +0200

    feature/VIH-6259 : Fixed review issues, added missing tests

--- TestApi/TestApi.IntegrationTests/Database/Queries/GetAllocationByUsernameQueryTests.cs
+++ TestApi/TestApi.IntegrationTests/Database/Queries/GetAllocationByUsernameQueryTests.cs
@@ -1,10 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using TestApi.DAL.Exceptions;
+using TestApi.DAL.Queries;
 
 namespace TestApi.IntegrationTests.Database.Queries
 {
-    class GetAllocationByUsernameQueryTests
+    public class GetAllocationByUsernameQueryTests : DatabaseTestsBase
     {
+        private readonly GetAllocationByUsernameQueryHandler _query;
+
+        public GetAllocationByUsernameQueryTests()
+        {
+            _query = new GetAllocationByUsernameQueryHandler(_dbContext);
+        }
+
+        [Test]
+        public async Task Should_return_allocation_by_username()
+        {
+            var user = await _context.TestDataManager.SeedUser();
+            var allocation = await _context.TestDataManager.SeedAllocation(user.Id);
+
+            var allocationDetails = await _query.Handle(new GetAllocationByUsernameQuery(user.Username));
+            allocationDetails.Should().BeEquivalentTo(allocation);
+        }
+
+        [Test]
+        public async Task Should_not_throw_error_for_nonexistent_username()
+        {
+            const string USERNAME = "made_up_username@email.com";
+            var allocationDetails = await _query.Handle(new GetAllocationByUsernameQuery(USERNAME));
+            allocationDetails.Should().BeNull();
+        }
+
+        [Test]
+        public async Task Should_not_throw_error_for_nonexistent_allocation()
+        {
+            var user = await _context.TestDataManager.SeedUser();
+            var allocationDetails = await _query.Handle(new GetAllocationByUsernameQuery(user.Username));
+            allocationDetails.Should().BeNull();
+        }
     }
 }
