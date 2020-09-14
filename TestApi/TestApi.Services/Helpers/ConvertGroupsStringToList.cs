d79b595262a9930b550c53459d624250bf6f7b33
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-09-14 12:44:53 +0200

    testfix/VIH-5255 : Fixed secrets issue

---
+++ TestApi/TestApi.Services/Helpers/ConvertGroupsStringToList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TestApi.Services.Helpers
+{
+    public static class ConvertGroupsStringToList
+    {
+        public static List<string> Convert(string text)
+        {
+            var groupsArrays = text.Split(",");
+            return groupsArrays.Select(@group => @group.Trim()).ToList();
+        }
+    }
+}
