23f939b106df00df131bd23a4a70263e6fb1e3e2
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-09-15 11:40:50 +0200

    feature/VIH-6268 : Change default organisation for admin web users

--- TestApi/TestApi.Common/Data/UserData.cs
+++ TestApi/TestApi.Common/Data/UserData.cs
@@ -7,8 +7,8 @@
         public const string PERFORMANCE_FIRST_NAME_PREFIX = "Performance_";
         public const string PROD_USER_SUFFIX = "_Prod";
         public const bool IS_PROD_USER = true;
-        public const bool IS_NOT_PROD_USER = false;
         public static readonly string MIDDLE_NAME = string.Empty;
+        public const string ORGANISATION = "MoJ";
         public const string REFERENCE = "Reference";
         public const string TELEPHONE_NUMBER = "+44(0)71234567891";
         public const string TITLE = "Mrs";
