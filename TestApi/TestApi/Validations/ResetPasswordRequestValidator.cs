6d891a2fe7d484c105849c5a08c96a668c350ce6
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-12-15 12:41:15 +0100

    testfix/VIH-5255 : Prevent Automation User Password Reset

---
+++ TestApi/TestApi/Validations/ResetPasswordRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentValidation;
+using TestApi.Common.Data;
+using TestApi.Contract.Requests;
+
+namespace TestApi.Validations
+{
+    public class ResetPasswordRequestValidator : AbstractValidator<ResetUserPasswordRequest>
+    {
+        private const string AUTOMATION_TEXT = UserData.AUTOMATED_FIRST_NAME_PREFIX;
+        public const string EMPTY_USERNAME_ERROR_MESSAGE = "You must supply a username";
+        public const string CANNOT_RESET_AUTOMATION_USERS_ERROR_MESSAGE = "Automation Users cannot be reset";
+
+        public ResetPasswordRequestValidator()
+        {
+            RuleFor(x => x.Username.Length)
+                .GreaterThan(0).WithMessage(EMPTY_USERNAME_ERROR_MESSAGE);
+
+            RuleFor(x => x.Username)
+                .Must(x => !x.Contains(AUTOMATION_TEXT, StringComparison.InvariantCultureIgnoreCase))
+                .WithMessage(CANNOT_RESET_AUTOMATION_USERS_ERROR_MESSAGE);
+        }
+    }
+}
