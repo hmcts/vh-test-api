f716039588610711378075c60b518094b2a62462
Levi <levi.butler@hmcts.net>
2021-01-25 15:52:59 +0000

    run coverage locally

---
+++ TestApi/run_coverage.bat
@@ -0,0 +1,9 @@
+rmdir /q /s Artifacts
+
+SET exclude=\"[TestApi.*Tests?]*,[TestApi.API]Startup,[TestApi.Common]*,[TestApi.Testing.Common]*\"
+dotnet test TestApi.IntegrationTests/VideoApi.IntegrationTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="\"opencover,cobertura,json,lcov\"" /p:CoverletOutput=../Artifacts/Coverage/ /p:MergeWith='../Artifacts/Coverage/coverage.json' /p:Exclude="${exclude}"
+dotnet test TestApi.UnitTests/TestApi.UnitTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="\"opencover,cobertura,json,lcov\"" /p:CoverletOutput=../Artifacts/Coverage/ /p:MergeWith='../Artifacts/Coverage/coverage.json' /p:Exclude="${exclude}"
+
+reportgenerator -reports:Artifacts/Coverage/coverage.opencover.xml -targetDir:Artifacts/Coverage/Report -reporttypes:HtmlInline_AzurePipelines
+
+"Artifacts/Coverage/Report/index.htm"
