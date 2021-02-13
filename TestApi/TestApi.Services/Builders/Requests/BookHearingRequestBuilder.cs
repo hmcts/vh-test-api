9ace1477195abc15f3aa79774405e8e238979658
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2021-02-13 17:12:00 +0100

    testfix/VIH-5255 : Improve user names

--- TestApi/TestApi.Services/Builders/Requests/BookHearingRequestBuilder.cs
+++ TestApi/TestApi.Services/Builders/Requests/BookHearingRequestBuilder.cs
@@ -60,10 +60,11 @@ namespace TestApi.Services.Builders.Requests
         {
             return _createHearingRequest.TestType switch
             {
+                TestType.Automated => HearingData.AUTOMATED_CASE_NAME_PREFIX,
+                TestType.Demo => HearingData.DEMO_CASE_NAME_PREFIX,
                 TestType.ITHC => HearingData.ITHC_CASE_NAME_PREFIX,
                 TestType.Manual => HearingData.MANUAL_CASE_NAME_PREFIX,
                 TestType.Performance => HearingData.PERFORMANCE_CASE_NAME_PREFIX,
-                TestType.Demo => HearingData.DEMO_CASE_NAME_PREFIX,
                 _ => HearingData.AUTOMATED_CASE_NAME_PREFIX
             };
         }
